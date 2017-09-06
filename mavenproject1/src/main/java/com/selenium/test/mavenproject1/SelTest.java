abd12497fa40e96abfacff44d4ead21df6f13f6c
OisinOwen <31633820+OisinOwen@users.noreply.github.com>
2017-09-06 11:23:44 +0100

    Add files via upload

--- mavenproject1/src/main/java/com/selenium/test/mavenproject1/SelTest.java
+++ mavenproject1/src/main/java/com/selenium/test/mavenproject1/SelTest.java
@@ -5,6 +5,9 @@
  */
 package com.selenium.test.mavenproject1;
 
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,11 +19,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class SelTest {
     public static void main(String[] args){
-//        System.setProperty("webdriver.firefox.marionette", "./geckodriver.exe");
-//        WebDriver driver = new FirefoxDriver();
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Oisin\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        String url = "http://www.bbc.co.uk";
+        driver.manage().window().setSize(new Dimension (1800, 1000));
+        String url = "http://www.valtech.com";
         driver.get(url);
+        
+        String content = driver.findElement(By.tagName("body")).getText();
+        Assert.assertTrue(content.contains("LATEST NEWS"));
+        
+        driver.findElement(By.linkText("ABOUT")).click();
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "About");
+        
+        driver.get(url);
+        driver.findElement(By.linkText("WORK")).click();
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "Work");
+
+        driver.get(url);
+        driver.findElement(By.linkText("SERVICES")).click();
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "Services");
+        
+        driver.get(url);
+        driver.findElement(By.cssSelector("i.icons.glyph")).click();
+        
+        int numberOfCities = driver.findElements(By.className("contactcities")).size();
+        System.out.println(numberOfCities);
+        
+        
+        
     }
 }
